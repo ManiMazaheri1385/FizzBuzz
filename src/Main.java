40cfc2ba58d6d6ec13e296bff155d015c07d75ac
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-03-16 14:53:09 +0330

    complete main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        for (int i = 1; i <= 100; i++) {
+            boolean printNumber = true;
+            if (i % 3 == 0) {
+                System.out.print("Fizz");
+                printNumber = false;
+            }
+            if (i % 5 == 0) {
+                System.out.print("Buzz");
+                printNumber = false;
+            }
+            if (!printNumber) {
+                System.out.println();
+                continue;
+            }
+            System.out.println(i);
+        }
     }
 }
